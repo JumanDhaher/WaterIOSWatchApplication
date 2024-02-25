5e692e10567df3faa645012c2d7f27ddb0dd896f
Juman <juman.developer@gmail.com>
2024-02-25 10:09:11 +0300

    Initial Commit

---
+++ WaterApplication/WaterApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApplicationApp.swift
+//  WaterApplication
+//
+//  Created by Juman Dhaher on 15/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
